6d996db4e2361be605ca57f794f89b7b0e3b7ec1
mamnasimjamaly <mamnasimjamaly@yahoo.com>
2026-08-24 14:19:31 -0700

    improvements etc

--- run-monitor.bat
+++ run-monitor.bat
@@ -4,6 +4,11 @@ cd /d "%~dp0app"
 
 if not exist "%~dp0logs" mkdir "%~dp0logs"
 
+if not exist "%~dp0venv\Scripts\python.exe" (
+    echo Missing venv\Scripts\python.exe. See README.md for setup.
+    exit /b 1
+)
+
 set HEADLESS=1
 set PYTHONIOENCODING=utf-8
 
